f9912fdec6b2fb3d673272fa4ad512593ed1da27
liljekvist <mikael0225@outlook.com>
2021-07-05 20:32:24 +0200

    Updatering till json controllern.

--- BackEnd/api/controllers/api_main.cc
+++ BackEnd/api/controllers/api_main.cc
@@ -6,9 +6,32 @@ using namespace api;
 void main::jsonTest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback){
     LOG_DEBUG<< "Test";
     Json::Value ret;
-    ret["test"]="ok";
-    ret["result"]="valid";
-    ret["token"]=drogon::utils::getUuid();
+    Json::Value user1;
+    Json::Value user2;
+    Json::Value user3;
+    Json::Value user4;
+    Json::Value user5;
+    user1["uid"]=1000;
+    user1["uuid"]=drogon::utils::getUuid();
+
+    user2["uid"]=12313;
+    user2["uuid"]=drogon::utils::getUuid();
+
+    user3["uid"]=321345;
+    user3["uuid"]=drogon::utils::getUuid();
+
+    user4["uid"]=167567;
+    user4["uuid"]=drogon::utils::getUuid();
+
+    user5["uid"]=98876;
+    user5["uuid"]=drogon::utils::getUuid();
+
+
+    ret["user1"]=user1;
+    ret["user2"]=user2;
+    ret["user3"]=user3;
+    ret["user4"]=user4;
+    ret["user5"]=user5;
     auto resp=HttpResponse::newHttpJsonResponse(ret);
     callback(resp);
 }
