1c0b3259fa924b9bbaef49180f513b6ae42453bd
YureTarded <christoffer.ljungahlstedt@gmail.com>
2021-07-05 22:39:10 +0200

    Get JSON array And setCookies

--- FrontEnd/script/script.js
+++ FrontEnd/script/script.js
@@ -1,6 +1,24 @@
 $(function() {
-    $.ajax({'url': "http://192.168.0.250:1000/json" ,dataType:'json',crossDomain:true,success:function(response){      
-        console.log(response.data); 
-      }
-   });
+    //First Time Page StartUp
+    /* Set coockies USE LATER
+    function setCookie(uid, uuid, date){
+        let expires = "";
+        if (date) {
+            const date = new Date();
+            date.setTime(date.getTime() + (date*24*60*60*1000))
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = uid + "=" + (uuid || "") + expires + "; path=/"
+    }
+    setCookie();
+    */
+
+    //Get UUID array
+    //From the URL
+    //Index is the UID and UUID
+    $.getJSON("http://192.168.0.250:1000/json", function(data){
+        $.each(data, function (index, value) {
+            console.log(value);
+        });
+    });
 })
