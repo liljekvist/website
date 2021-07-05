a896d1c306158d76b5d6e27d698a8b952b07aab5
liljekvist <mikael0225@outlook.com>
2021-07-05 23:26:22 +0200

    Get posts för startpage

--- BackEnd/api/controllers/api_main.h
+++ BackEnd/api/controllers/api_main.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <drogon/HttpController.h>
+#include <cstring>
+//#include <vector>
 using namespace drogon;
 namespace api
 {
@@ -11,12 +13,16 @@ class main:public drogon::HttpController<main>
     //METHOD_ADD(main::get,"/{2}/{1}",Get);//path is /api/main/{arg2}/{arg1}
     //METHOD_ADD(main::your_method_name,"/{1}/{2}/list",Get);//path is /api/main/{arg1}/{arg2}/list
     ADD_METHOD_TO(main::jsonTest,"/json",Get);//path is /absolute/path/{arg1}/{arg2}/list
-
+    ADD_METHOD_TO(main::getPosts,"/getPosts",Get);
+    //ADD_METHOD_TO(main::jsonTest,"/json",Get);
+    //ADD_METHOD_TO(main::jsonTest,"/json",Get);
 
     METHOD_LIST_END
     // your declaration of processing function maybe like this:
     // void get(const HttpRequestPtr& req,std::function<void (const HttpResponsePtr &)> &&callback,int p1,std::string p2);
     // void your_method_name(const HttpRequestPtr& req,std::function<void (const HttpResponsePtr &)> &&callback,double p1,int p2) const;
     void jsonTest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback);
+    void getPosts(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback);
+    
 };
 }
