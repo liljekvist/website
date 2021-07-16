ff26b4491da782f94c933963d212c76b962a2a2b
YureTarded <christoffer.ljungahlstedt@gmail.com>
2021-07-16 14:05:57 +0200

    Able to view posts

--- FrontEnd/script/website-post-site.js
+++ FrontEnd/script/website-post-site.js
@@ -1,9 +1,26 @@
 $(function(){
     const id = window.location.search.substring(4);
-    console.log(id);
-    $.get("http://192.168.0.250:1000/post", {postid: id});
-
-    function showPost(title, msg){
-        
+    try {
+        $.ajax({
+            url: 'http://192.168.0.250:1000/post?postid=' + id,
+            type: 'GET',
+            success: function(result){
+                printPost(result);
+            }
+        })
+    } catch (error) {
+        console.log('Micke är stinky och detta är felet: ' + error);
+        return null;
     }
+    function printPost(obj) {
+        const postDate = obj.date;
+        const postTitle = obj.title;
+        const postText = obj.msg;
+        const pageDate = document.getElementById('date');
+        const pageTitle = document.getElementById('title');
+        const pageText = document.getElementById('text');
+        pageDate.appendChild(document.createTextNode('Date: ' + postDate));
+        pageTitle.appendChild(document.createTextNode(postTitle));
+        pageText.appendChild(document.createTextNode(postText));
+    };
 });
