9eb76dbcf77acd930f917609d96b31aec8ac713f
YureTarded <christoffer.ljungahlstedt@gmail.com>
2021-08-02 14:56:40 +0200

    Working on comments but it does not work as I intend

--- FrontEnd/script/website-post-site.js
+++ FrontEnd/script/website-post-site.js
@@ -1,5 +1,6 @@
 $(function(){
     const id = window.location.search.substring(4);
+    /**Get and show post and existing comments*/
     try {
         $.ajax({
             url: 'http://192.168.0.250:1000/post?postid=' + id,
@@ -7,6 +8,11 @@ $(function(){
             success: function(result){
                 printPost(result);
             }
+        })
+        $.getJson('http://192.168.0.250:1000/json/getComments', function(comment){
+            $.each(comment, function(value) {
+                printComment(value);
+            })
         })
     } catch (error) {
         console.log('Micke är stinky och detta är felet: ' + error);
@@ -23,4 +29,23 @@ $(function(){
         pageTitle.appendChild(document.createTextNode(postTitle));
         pageText.appendChild(document.createTextNode(postText));
     };
+    function printComment(obj) {
+        const commentMsg = obj.msg;
+        const commentDate = obj.date;
+        const commentSection = document.getElementById('commentSection');
+        commentSection.appendChild(document.createTextNode('Date: ', + commentDate));
+        commentSection.appendChild(document.createTextNode(commentMsg));
+    }
+    $('#comment').submit(function(){
+        localStorage.setItem('text', 'test');
+        //const uid = sessionStorage.getItem(uid);
+        const comment = $('#cInput').val();
+        /*
+        $.ajax({
+            url: 'http://192.168.0.250:1000/json/makeComment?uid=' + uid + '&message=' + comment,
+            type: 'POST'
+        })
+        */
+        return false;
+    })
 });
